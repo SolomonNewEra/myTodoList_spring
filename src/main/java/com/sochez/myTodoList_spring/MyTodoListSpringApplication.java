4aa5a358e1a06f0503974321a756c0d5b71d1e40
SolomonNewEra <ezeatumsolomon@gmail.com>
2023-12-05 04:22:22 +0100

    first commit

---
+++ src/main/java/com/sochez/myTodoList_spring/MyTodoListSpringApplication.java
@@ -0,0 +1,13 @@
+package com.sochez.myTodoList_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTodoListSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTodoListSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sochez/myTodoList_spring/MyTodoListSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sochez.myTodoList_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTodoListSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
