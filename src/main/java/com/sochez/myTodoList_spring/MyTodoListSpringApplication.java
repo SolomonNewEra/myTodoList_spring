0beb235c6d7329093b37f3768490e6217babbd01
Eric Opoku <20393990+ericus20@users.noreply.github.com>
2023-12-06 10:53:18 -0500

    Update MyTodoListSpringApplication.java

--- src/main/java/com/sochez/myTodoList_spring/MyTodoListSpringApplication.java
+++ src/main/java/com/sochez/myTodoList_spring/MyTodoListSpringApplication.java
@@ -1,33 +1,13 @@
 package com.sochez.myTodoList_spring;
 
-import com.sochez.myTodoList_spring.entity.Todo;
-import com.sochez.myTodoList_spring.repository.TodoRepository;
-import com.sochez.myTodoList_spring.service.TodoServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyTodoListSpringApplication implements CommandLineRunner {
-	@Autowired
-	private TodoRepository todoRepository;
-	private TodoServiceImpl todoServiceImpl;
+public class MyTodoListSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyTodoListSpringApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Todo todo = new Todo();
-
-		todo.setTask("Buy book");
-		todo.setCompleted(false);
-		todoRepository.save(todo);
-		//todoServiceImpl.findById();
-
-
-
 	}
 }
